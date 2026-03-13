876660587d2e6c335d37943875ca13d35ecc302c
Roman <WizardPronomuos@yandex.ru>
2026-03-13 18:08:16 +0300

    Fix: Serve app from root domain

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/">
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
